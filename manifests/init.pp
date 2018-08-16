5bd2a124b59b44aaf74a8bcb0d859356a77e9b00
Jordi Prats <jordi.prats@gmail.com>
2018-08-16 11:34:53 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-class ansible(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits ansible::params{
+class ansible (
+                $manage_package        = true,
+                $package_ensure        = 'installed',
+                $manage_service        = true,
+                $manage_docker_service = true,
+                $service_ensure        = 'running',
+                $service_enable        = true,
+              ) inherits ansible::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
