33be5fcf96c93ae269114c1ad76c94e9073b182f
Jordi Prats <jordi.prats@gmail.com>
2018-08-16 13:26:26 +0200

    typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,11 @@
 class ansible::config inherits ansible {
 
   file { '/etc/ansible/ansible.cfg':
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    cotent => template("${module_name}/ansiblecfg.erb"),
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/ansiblecfg.erb"),
   }
 
 }
